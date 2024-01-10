b0b169a4f803e0a1ee2e6763f4a82e44110c0d28
Noor Mohammed <noormohammedknr2@gmail.com>
2024-01-10 19:56:32 +0530

    bug fixes

--- src/Components/TodoList/TodoList.jsx
+++ src/Components/TodoList/TodoList.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import "./TodoList.css";
 
 const TodoList = ({ todos, deleteHandler }) => {
+  console.log("todos are", todos);
   return (
     <div className="todos">
       {todos.map((todo, index) => {
         return (
           <h3 key={index} className="todolist-body">
-            {todo.title} &nbsp;{" "}
+            {todo}
             <button onClick={() => deleteHandler(index)}>Delete</button>
           </h3>
         );
